942ff8d95c2e7ee1acb76c46cc86aee8a3f89786
emmanuel-kipkiru <emmanuelkipkirui17@gmail.com>
2022-08-22 00:44:55 +0300

    commit tree 1c6f78c679771d64099a6bdc74d9670b3cfb217c

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,17 @@
 # Your code goes here!
+class Anagram
+
+
+    attr_accessor :name
+
+
+
+    def initialize(text)
+        @name=text
+
+    end
+    def match(arr)
+        arr.select {|i| i.split("").sort==@name.split("").sort}
+
+    end
+end
